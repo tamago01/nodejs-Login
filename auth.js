e0e12d9e66a0632a3812f1fa75f2475dba346ed7
Pradeep Lama Dimdung <tamangjake@gmai.com>
2023-10-01 00:05:28 +0545

    minor edit

--- auth.js
+++ auth.js
@@ -1,30 +1,30 @@
-import GoogleStrategy from "passport-google-oauth20";
+import GithubStrategy from 'passport-github2';
 import passport from "passport";
 import "dotenv/config";
 
-const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } = process.env;
+const { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } = process.env;
 passport.use(
-  new GoogleStrategy(
+  new GithubStrategy(
     {
-      clientID: GOOGLE_CLIENT_ID,
-      clientSecret: GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:8000/auth/google/callback",
+      clientID: GITHUB_CLIENT_ID,
+      clientSecret: GITHUB_CLIENT_SECRET,
+      callbackURL: "http://localhost:8000/auth/github/callback",
     },
     function (accessToken, refreshToken, profile, cb) {
-      //   console.log("hello");
-      //   User.findOrCreate({ googleId: profile.id }, function (err, user) {
+        //  console.log("hello");
+        User.findOrCreate({ accountId: profile.id, provider: 'github' }, function (err, user) {
 
-      console.log(profile);
+      // console.log(profile);
       return cb(null, profile);
-      //   });
+        });
     }
   )
 );
 
-passport.serializeUser(function (user, cb) {
-  cb(null, user);
-});
+// passport.serializeUser(function (user, cb) {
+//   cb(null, user);
+// });
 
-passport.deserializeUser(function (user, cb) {
-  cb(null, user);
-});
+// passport.deserializeUser(function (user, cb) {
+//   cb(null, user);
+// });
--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 import express from 'express';
+import passport from "passport";
+import "./auth.js";
 
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("backend is working!");
+    res.send("<a href='auth/github'>Signup with Github </a>");
 })
+
+app.get(
+    "/auth/github",
+    // console.log('hello')
+    // passport.authenticate("github", {scope: ["user:email"]})
+    );
+
+app.get("/auth/github/callback", (req, res) => {
+    passport.authenticate("github", (req, res) => {
+        successRedirect("http://localhost:3000");
+        failureRedirect("/auth/errpr");
+
+    })
+
+});
 
 app.listen(8000, () => {
     console.log('Server running on port 8000'); 
